1fa07335893790920b8d351457b66c314c4db825
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-09 01:43:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,11 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
   
-  GENRES = [ ]
+  GENRES = []
 
   def initialize(title)
     @title = title
   end
-  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
