c514f5571868f62ebc061ea86b5b899e35e373a1
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-09 01:44:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,8 +4,12 @@ class Shoe
   
   BRANDS = [ ]
 
-  def initialize(brand)
+   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
